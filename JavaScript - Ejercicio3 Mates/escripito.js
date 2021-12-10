107c7884e92aa5408a11f3cf9c6fa0d7d13d9f2b
Ismael Cabral <92926022+ismaaa3@users.noreply.github.com>
2021-12-10 13:51:24 +0100

    Add files via upload

---
+++ JavaScript - Ejercicio3 Mates/escripito.js
@@ -0,0 +1,35 @@
+/* 
+	Exemplo ficheiro (a completar) en JavaScript
+	Nome do ficheiro: escripito.js
+	 
+*/
+
+var moito = 9.845 ;
+    var raiz = Math.sqrt(moito);
+    alert(raiz);
+
+var mucho = 7.542 ;
+ 	var raiz2 = Math.sqrt(mucho);
+    alert(raiz2);
+
+var muchisimo = 3.296 ;
+    var raiz3 = Math.sqrt(muchisimo);
+    alert(raiz3);
+
+
+var arriba = 59 ;
+    var cuadrado = Math.pow(arriba, 2);
+    alert(cuadrado);
+
+var abajo = Math.PI ;
+    var pi = Math.trunc(Math.PI);
+    alert(pi);
+ 
+var bajo = Math.min ;
+    var peque = Math.min(moito, mucho, muchisimo);
+    alert(peque);
+ 
+var aleatorio = Math.random ;
+    var cualquiera = Math.random(0, 3);
+    alert(cualquiera);
+ 
